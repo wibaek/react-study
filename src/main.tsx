2cb02d186fe3b9e4fd4e875e565968f9e719910d
Wibaek Park <wibaek@ninewatt.com>
2024-10-15 11:20:59 +0900

    design: RootPage styled component 적용, global css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 import RootPage from './pages/RootPage';
 
 const router = createBrowserRouter([
--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -1,29 +1,31 @@
+import { SearchSpinner, Sidebar, SidebarButton, SidebarForm, SidebarTitle, SidebarSearch, SrOnly, SidebarHeader, SidebarNav, SidebarNavList, SidebarNavItem, SidebarNavLink } from './styles';
+
 const RootPage = () => {
   return (
     <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Sidebar id="sidebar">
+        <SidebarTitle>React Router Contacts</SidebarTitle>
+        <SidebarHeader>
+          <SidebarForm id="search-form" role="search">
+            <SidebarSearch id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
+            <SearchSpinner id="search-spinner" aria-hidden hidden={true} />
+            <SrOnly className="sr-only" aria-live="polite"></SrOnly>
+          </SidebarForm>
+          <SidebarForm method="post">
+            <SidebarButton type="submit">New</SidebarButton>
+          </SidebarForm>
+        </SidebarHeader>
+        <SidebarNav>
+          <SidebarNavList>
+            <SidebarNavItem>
+              <SidebarNavLink href={`/contacts/1`}>Your Name</SidebarNavLink>
+            </SidebarNavItem>
+            <SidebarNavItem>
+              <SidebarNavLink href={`/contacts/2`}>Your Friend</SidebarNavLink>
+            </SidebarNavItem>
+          </SidebarNavList>
+        </SidebarNav>
+      </Sidebar>
       <div id="detail"></div>
     </>
   );
